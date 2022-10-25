e5176b11518b5c01d041c4d04a95a61d2a3c456b
Joslynn <joslynnkim@gmail.com>
2022-10-25 17:55:13 +0900

    김보경_#12_sellDao 변경

--- src/main/java/luxe/dao/sell/SellDAO.java
+++ src/main/java/luxe/dao/sell/SellDAO.java
@@ -8,70 +8,81 @@ import luxe.dto.BidDTO;
 import luxe.dto.SellDTO;
 
 public interface SellDAO {
-	
+
 	/**
 	 * 판매 신청등록
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
 	int insertSell(SellDTO sell) throws SQLException;
-	
-	
+
 	/**
 	 * 판매상품 가격수정
+	 * 
 	 * @param sellNo
 	 * @return
 	 * @throws SQLException
 	 */
 	int updateSellPrice(SellDTO sellDTO) throws SQLException;
-	
-	
+
 	/**
-	 * 관리자 판매신청 승인단계 
+	 * 관리자 판매신청 승인단계
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
 	int updateSellStatus(SellDTO sellDTO) throws SQLException;
-	
-	
+
+	/**
+	 * 판매 상태 완료로 변경
+	 * 
+	 * @param sellNo
+	 * @return
+	 * @throws SQLException
+	 */
+	int UpdateSellStatusComplete(int sellNo) throws SQLException;
+
 	/**
 	 * 관리자가 판매신청한 모든 유저의 정보를 조회한다.
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
 	List<SellDTO> selectSellAll() throws SQLException;
-	
+
 	/**
 	 * 판매중 상품명, 브랜드명, 진행상태, 거래가, 판매승인일
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
 	List<SellDTO> selectSellingInfoByUserId(String userId) throws SQLException;
-		
-	
+
 	/**
 	 * 판매대기중 상품명 ,브랜드명, 진행상태, 거래가, 판매신청일
+	 * 
 	 * @param userId
 	 * @return
 	 * @throws SQLException
 	 */
 	List<SellDTO> selectSellWaitInfoByUserId(String userId) throws SQLException;
-	
-	
+
 	/**
 	 * 최저 판매가격 조회
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
 	SellDTO selectLowestPriceByGoodsNo(int goodsNo) throws SQLException;
-	
-	
+
 	/**
 	 * sellNo를 이용해 goodsNo를 구한다.
+	 * 
 	 * @param sellNo
 	 * @return
 	 * @throws SQLException
 	 */
-	
+
 	public boolean sellDuplicateCheck(SellDTO sellDTO) throws SQLException;
 }
