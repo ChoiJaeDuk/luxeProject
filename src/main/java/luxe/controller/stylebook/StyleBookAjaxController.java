f458388687cc96695b19b8992ffef8311670dc88
jieun <jieun3604@gmail.com>
2022-10-25 18:49:54 +0900

    임지은_#18 stylebookajaxcontroller 수정

--- src/main/java/luxe/controller/stylebook/StyleBookAjaxController.java
+++ src/main/java/luxe/controller/stylebook/StyleBookAjaxController.java
@@ -6,16 +6,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import luxe.controller.AjaxController;
 import luxe.controller.Controller;
 import luxe.controller.ModelAndView;
 
-public class StyleBookAjaxController implements Controller {
+public class StyleBookAjaxController implements AjaxController {
 
 	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 }
